05b31cef14abf0b109ce282779ae5fb22307fe94
Mark Stiles <stiles.mark@gmail.com>
2019-07-05 21:24:22 -0400

    * removing duplicate area registration

--- src/Foundation/SCSDK/code/App_Start/RegisterArea.cs
+++ src/Foundation/SCSDK/code/App_Start/RegisterArea.cs
@@ -7,40 +7,6 @@ using Sitecore.Pipelines;
 
 namespace SitecoreCognitiveServices.Foundation.SCSDK
 {
-    public class RegisterArea
-    {
-        public virtual void Process(PipelineArgs args)
-        {
-            var car = new CognitiveAreaRegistration();
-
-            try
-            {
-                List<RouteBase> list = RouteTable
-                    .Routes
-                    .Where(route =>
-                    {
-                        Route route1 = route as Route;
-                        return route1?.Defaults != null && route1.Defaults.ContainsKey("area") 
-                            && string.Equals(
-                                route1.Defaults["area"].ToString(), 
-                                car.AreaName, 
-                                StringComparison.Ordinal);
-                    })
-                    .ToList();
-
-                if (!list.Any())
-                {
-                    car.RegisterArea(new AreaRegistrationContext(car.AreaName, RouteTable.Routes));
-                    return;
-                }
-
-                foreach (Route route in list)
-                    route.DataTokens["UseNamespaceFallback"] = true;
-            }
-            catch (Exception ex) { }
-        }
-    }
-
     public class CognitiveAreaRegistration : AreaRegistration
     {
         public override string AreaName => "SitecoreCognitiveServices";
