128a4efbe1753ee788ea44ef17db8caf8f24cc60
Mark Stiles <stiles.mark@gmail.com>
2019-12-30 12:47:38 -0500

    * wiring up the personalizer service classes

--- src/Foundation/SCSDK/code/Services/MSSDK/Decision/IPersonalizerService.cs
+++ src/Foundation/SCSDK/code/Services/MSSDK/Decision/IPersonalizerService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SitecoreCognitiveServices.Foundation.MSSDK.Decision.Models.Personalizer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,24 @@ namespace SitecoreCognitiveServices.Foundation.SCSDK.Services.MSSDK.Decision
 {
     public interface IPersonalizerService
     {
+        RankResponse Rank(RankRequest request);
+        ClientConfiguration GetClientConfiguration();
+        void RewardTopRankedAction(string eventId);
+        void ActivateEvent(string eventId);
+        Evaluation SubmitNewEvaluation(EvaluationRequest request);
+        Policy DeleteCurrentPolicy();
+        void DeleteEvaluation(string evaluationId);
+        void DeleteLogs();
+        void DeleteModel();
+        Policy UpdatePolicyConfiguration(Policy policy);
+        Service UpdateServiceConfiguration(Service config);
+        byte[] GetCurrentModel();
+        ModelProperties GetModelProperties();
+        Evaluation GetEvaluation(string evaluationId);
+        Policy GetPolicyConfiguration();
+        Service GetServiceConfiguration();
+        LogsProperties GetLogsProperties();
+        List<Evaluation> GetEvaluations();
+        Status GetStatus();
     }
 }
